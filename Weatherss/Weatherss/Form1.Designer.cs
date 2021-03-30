4416dc3b1a32218eae7d7e8afa3fb48d8a2e1876
phuocnguyen0717 <pnhesi@gmail.com>
2021-03-30 16:27:06 +0700

    API

---
+++ Weatherss/Weatherss/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace Weatherss
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_textCity = new System.Windows.Forms.Label();
+            this.lbl_textCountry = new System.Windows.Forms.Label();
+            this.lbl_DoCe = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_Day = new System.Windows.Forms.Label();
+            this.lbl_Conditions = new System.Windows.Forms.Label();
+            this.lbl_Description = new System.Windows.Forms.Label();
+            this.lbl_Des2 = new System.Windows.Forms.Label();
+            this.lbl_Speed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_textCity
+            // 
+            this.lbl_textCity.AutoSize = true;
+            this.lbl_textCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_textCity.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbl_textCity.Location = new System.Drawing.Point(12, 9);
+            this.lbl_textCity.Name = "lbl_textCity";
+            this.lbl_textCity.Size = new System.Drawing.Size(318, 67);
+            this.lbl_textCity.TabIndex = 0;
+            this.lbl_textCity.Text = "Thành Phố";
+            // 
+            // lbl_textCountry
+            // 
+            this.lbl_textCountry.AutoSize = true;
+            this.lbl_textCountry.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_textCountry.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbl_textCountry.Location = new System.Drawing.Point(12, 112);
+            this.lbl_textCountry.Name = "lbl_textCountry";
+            this.lbl_textCountry.Size = new System.Drawing.Size(266, 67);
+            this.lbl_textCountry.TabIndex = 0;
+            this.lbl_textCountry.Text = "Quốc gia";
+            // 
+            // lbl_DoCe
+            // 
+            this.lbl_DoCe.AutoSize = true;
+            this.lbl_DoCe.Font = new System.Drawing.Font("Microsoft Sans Serif", 46F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_DoCe.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lbl_DoCe.Location = new System.Drawing.Point(12, 231);
+            this.lbl_DoCe.Name = "lbl_DoCe";
+            this.lbl_DoCe.Size = new System.Drawing.Size(93, 87);
+            this.lbl_DoCe.TabIndex = 0;
+            this.lbl_DoCe.Text = "C";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(437, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(227, 67);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Forcast";
+            // 
+            // lbl_Day
+            // 
+            this.lbl_Day.AutoSize = true;
+            this.lbl_Day.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Day.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lbl_Day.Location = new System.Drawing.Point(441, 112);
+            this.lbl_Day.Name = "lbl_Day";
+            this.lbl_Day.Size = new System.Drawing.Size(111, 46);
+            this.lbl_Day.TabIndex = 0;
+            this.lbl_Day.Text = "Days";
+            this.lbl_Day.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lbl_Conditions
+            // 
+            this.lbl_Conditions.AutoSize = true;
+            this.lbl_Conditions.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Conditions.ForeColor = System.Drawing.Color.Green;
+            this.lbl_Conditions.Location = new System.Drawing.Point(441, 183);
+            this.lbl_Conditions.Name = "lbl_Conditions";
+            this.lbl_Conditions.Size = new System.Drawing.Size(210, 46);
+            this.lbl_Conditions.TabIndex = 0;
+            this.lbl_Conditions.Text = "Conditions";
+            // 
+            // lbl_Description
+            // 
+            this.lbl_Description.AutoSize = true;
+            this.lbl_Description.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Description.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Description.Location = new System.Drawing.Point(441, 253);
+            this.lbl_Description.Name = "lbl_Description";
+            this.lbl_Description.Size = new System.Drawing.Size(221, 46);
+            this.lbl_Description.TabIndex = 0;
+            this.lbl_Description.Text = "Description";
+            // 
+            // lbl_Des2
+            // 
+            this.lbl_Des2.AutoSize = true;
+            this.lbl_Des2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Des2.ForeColor = System.Drawing.Color.DarkGoldenrod;
+            this.lbl_Des2.Location = new System.Drawing.Point(677, 231);
+            this.lbl_Des2.Name = "lbl_Des2";
+            this.lbl_Des2.Size = new System.Drawing.Size(64, 46);
+            this.lbl_Des2.TabIndex = 0;
+            this.lbl_Des2.Text = "26";
+            this.lbl_Des2.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lbl_Speed
+            // 
+            this.lbl_Speed.AutoSize = true;
+            this.lbl_Speed.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Speed.ForeColor = System.Drawing.Color.Fuchsia;
+            this.lbl_Speed.Location = new System.Drawing.Point(631, 112);
+            this.lbl_Speed.Name = "lbl_Speed";
+            this.lbl_Speed.Size = new System.Drawing.Size(150, 46);
+            this.lbl_Speed.TabIndex = 0;
+            this.lbl_Speed.Text = "12km/h";
+            this.lbl_Speed.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_textCountry);
+            this.Controls.Add(this.lbl_DoCe);
+            this.Controls.Add(this.lbl_Description);
+            this.Controls.Add(this.lbl_Conditions);
+            this.Controls.Add(this.lbl_Des2);
+            this.Controls.Add(this.lbl_Speed);
+            this.Controls.Add(this.lbl_Day);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_textCity);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_textCity;
+        private System.Windows.Forms.Label lbl_textCountry;
+        private System.Windows.Forms.Label lbl_DoCe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_Day;
+        private System.Windows.Forms.Label lbl_Conditions;
+        private System.Windows.Forms.Label lbl_Description;
+        private System.Windows.Forms.Label lbl_Des2;
+        private System.Windows.Forms.Label lbl_Speed;
+    }
+}
+
---
+++ Weatherss/Weatherss/Form1.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Weatherss
+{
+    public partial class Form1 : Form
+    {
+        const string APPID = "585b2ee17dabee43a31f5d5e510baa4d";
+        string cityID = "1566083";
+        public Form1()
+        {
+            InitializeComponent();
+           // getWeather("1566083");
+            //geForast("1566083");
+
+        }
+        //void getWeather(string city)
+        //{
+        //    using (WebClient web = new WebClient())
+        //    {
+        //        string url = string.Format("http://api.openweathermap.org/data/2.5/weather?id={0}&appid={1}", city,APPID);
+
+        //        var json = web.DownloadString(url);
+
+        //        var result = JsonConvert.DeserializeObject<WeatherInfo.root>(json);
+        //        WeatherInfo.root outPut = result;
+
+        //        lbl_textCity.Text = string.Format("{0}", outPut.name);
+        //        lbl_textCountry.Text = string.Format("{0}", outPut.sys.country);
+        //        lbl_DoCe.Text = string.Format("{0} \u00B0", outPut.main.temp)+ "*c";
+        //    }
+        //}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        //void geForast(string city)
+        //{
+        //    string url = "";
+        //    using (WebClient web = new WebClient())
+        //    {
+        //        var json = web.DownloadString(url);
+        //        var Object = JsonConvert.DeserializeObject<WeatherForcasts>(json);
+        //        WeatherForcasts forcasts = Object;
+        //        lbl_Conditions.Text = string.Format("{0}", forcasts.list[1].weather[0].main);
+        //        lbl_Description.Text = string.Format("{0}", forcasts.list[1].weather[0].descriptions);
+        //        lbl_Des2.Text = string.Format("{0}  \u00B0", forcasts.list[1].temp);
+        //        lbl_Speed.Text = string.Format("{0}  \u00B0", forcasts.list[1].speed);
+        //    }
+
+        //}
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+             
+        }
+    }
+}
